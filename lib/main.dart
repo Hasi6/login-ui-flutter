f6d6ad971dd01416c5d32b0c1907e3121119ce23
Hasi6 <37216970+Hasi6@users.noreply.github.com>
2019-10-21 17:08:37 +0530

    create first page

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:login_ui/ui/HomePage/HomePage.dart';
+
+void main() => runApp(
+    new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title:'Login App',
+      home: HomaPage(),
+    )
+);
---
+++ lib/ui/HomePage/HomePage.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class HomaPage extends StatefulWidget {
+  @override
+  _HomaPageState createState() => _HomaPageState();
+}
+
+class _HomaPageState extends State<HomaPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("images/bg3.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(30.0),
+            ),
+            Center(
+              child: Image.asset('./images/app_logo.png', width: 150.0,),
+            ),
+            Expanded(
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: SizedBox(
+                  width: double.infinity,
+                  child: new RaisedButton(
+                    color: Colors.blueAccent,
+                    child: Text("Login", style: TextStyle(color: Colors.white),),
+                    onPressed: ()=>loginBtnClicked(),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ), /* add child content here */
+      ),
+    );
+  }
+}
+
+loginBtnClicked(){
+  print("Hasi");
+}
